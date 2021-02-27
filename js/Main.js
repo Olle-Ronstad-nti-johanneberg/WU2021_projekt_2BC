c1c076705ee81eabf6114fb965a60399c13c82d9
Olle Ronstad <Olle.Ronstad@gmail.com>
2021-02-27 23:11:21 +0100

    fixed Javascript problem. Changed color theme. reworked nav for desktop. changed some text in html

--- js/Main.js
+++ js/Main.js
@@ -3,8 +3,12 @@ function showMenu() {
     let menu = document.querySelector("nav");
     menu.classList.toggle("LargeNav")
 
-    let menuitem = document.querySelector(".navitem")
-    menuitem.classList.toggle(".navitemshow")
+    const nodes = document.querySelectorAll (".navitem");
+
+    //this code is quite weird to keep it compatible with internet explorer 6
+    Array.prototype.forEach.call (nodes, node => {
+    node.classList.toggle ("navitemshow")
+    })
 
 }
 let menuButton = document.querySelector("nav")
