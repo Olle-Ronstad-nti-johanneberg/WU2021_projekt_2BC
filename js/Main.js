79c47df3203d34b5c1f7c7ac31873609d085e259
Olle Ronstad <Olle.Ronstad@gmail.com>
2021-03-02 11:54:14 +0100

    added sketches for 3d artwork, stated to work on artwork page. devided mainpage into more classes for more control

--- js/Main.js
+++ js/Main.js
@@ -1,15 +1,15 @@
 
 function showMenu() {
     let menu = document.querySelector("nav");
-    menu.classList.toggle("LargeNav")
+    menu.classList.toggle("LargeNav");
 
     const nodes = document.querySelectorAll (".navitem");
 
     //this code is quite weird to keep it compatible with internet explorer 6
     Array.prototype.forEach.call (nodes, node => {
-    node.classList.toggle ("navitemshow")
+    node.classList.toggle ("navitemshow");
     })
 
 }
-let menuButton = document.querySelector("nav")
-menuButton.addEventListener("click",showMenu)
+let menuButton = document.querySelector("nav");
+menuButton.addEventListener("click",showMenu);
